16d38ae5acaf719719dd9d6e6938388d3fa50a18
Romullosilva <PedroDRMenezes@gmail.com>
2020-01-09 13:33:02 -0300

    Atualizações do Angular

--- projeto/src/app/model/postagem.ts
+++ projeto/src/app/model/postagem.ts
@@ -1,8 +1,3 @@
-/*[{"idPostagem":1,
-"titulo":"Campeaaaaaao",
-"texto":"Brasil vence a Copa do Mundo!",
-"linkimg":"https://1.bp.blogspot.com/-n0-rtPF1ilE/WtNZ68Qkm2I/AAAAAAAADNU/cm9TizzME9Q88SXx2LBa4RaQ884PfufJwCLcBGAs/s1600/QUINZE%2BDE%2BJAU%2B-%2B3.jpg","dataInclusao":"10/07/1958"}
-*/
 
 export class Postagem{
 
