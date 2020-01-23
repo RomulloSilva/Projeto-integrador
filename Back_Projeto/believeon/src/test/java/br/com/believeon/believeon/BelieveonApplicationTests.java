d15e8e99c814d914098ab1c5611867699d329e41
Romullosilva <romullo.dsilva@gmail.com>
2020-01-23 14:19:07 -0300

    back

---
+++ Back_Projeto/believeon/src/test/java/br/com/believeon/believeon/BelieveonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.believeon.believeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelieveonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
